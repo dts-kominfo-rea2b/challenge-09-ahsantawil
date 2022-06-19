b0fbcf9305dd45da5b44708668ba24c188134627
ahsantawil <ahsan.tawil07@gmail.com>
2022-06-19 08:50:01 +0700

    Challenge 09 - React

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,19 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css';
 
-const Contact = () => {
+const Contact = (props) => {
+    const {data} = props;
     return (
-        <></>
+        <div className='card'>
+            <img className='card-img' src={data.photo} alt={data.name} />
+            <div className='card-body'>
+                <h3>{data.name}</h3>
+                <span>{data.phone}</span>
+                <span>{data.email}</span>
+            </div>            
+        </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
-        <></>
+            <div className='header'>
+                <h2> Call a Friend</h2>
+                <div>
+                    <span>Your Friendly Contact App</span>
+                </div>
+            </div>
     )
 }
 
